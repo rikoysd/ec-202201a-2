c1b3de0db83878099df70fc4a6c01a4cb3fbd00f
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-07 23:04:48 +0900

    モックの貼り付け

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,17 +1,156 @@
 <template>
   <div>
-    
+    <div class="search-wrapper">
+      <div class="container">
+        <form method="post" class="search-form">
+          <input type="text" name="name" class="search-name-input" />
+
+          <button class="btn search-btn" type="button">
+            <span>検索</span>
+          </button>
+        </form>
+      </div>
+    </div>
+
+    <!-- item list -->
+    <div class="item-wrapper">
+      <div class="container">
+        <div class="items">
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/1.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/2.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/3.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/4.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/5.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/6.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/7.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/8.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/9.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/10.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/11.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/12.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/13.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+
+          <div class="item">
+            <div class="item-icon">
+              <img src="img/14.jpg" />
+            </div>
+            <a href="item_detail.html">ハワイアンパラダイス</a><br />
+            <span class="price">Ｍ</span>1,380円(税抜)<br />
+            <span class="price">Ｌ</span>2,380円(税抜)<br />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class XXXComponent extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
