3aea6935e26e5266ab85346ff571fa1008b76562
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-08 11:13:28 +0900

    pathの修正

--- src/views/itemDetail.vue
+++ src/views/itemDetail.vue
@@ -1,17 +1,130 @@
 <template>
   <div>
-    
+    <div class="top-wrapper">
+      <div class="container">
+        <h1 class="page-title">Hawaiianパラダイス</h1>
+        <div class="row">
+          <div class="row item-detail">
+            <div class="item-icon">
+              <img src="img/1.jpg" />
+            </div>
+            <div class="item-intro">
+              ハワイで取れる名産物でかつオーガニックな食料がふんだんに使われたローカルフーズです。健康志向の方に大人気の商品です。
+            </div>
+          </div>
+          <div class="row item-size">
+            <div class="item-hedding">サイズ</div>
+            <div>
+              <label>
+                <input id="size-m" name="size" type="radio" checked="checked" />
+                <span>
+                  &nbsp;<span class="price">Ｍ</span
+                  >&nbsp;&nbsp;1,380円(税抜)</span
+                >
+              </label>
+              <label>
+                <input id="size-l" name="size" type="radio" />
+                <span>
+                  &nbsp;<span class="price">Ｌ</span
+                  >&nbsp;&nbsp;2,380円(税抜)</span
+                >
+              </label>
+            </div>
+          </div>
+          <div class="row item-toppings">
+            <div class="item-hedding">
+              トッピング：&nbsp;1つにつき
+              <span>&nbsp;Ｍ&nbsp;</span>&nbsp;&nbsp;200円(税抜)
+              <span>&nbsp;Ｌ</span>&nbsp;&nbsp;300円(税抜)
+            </div>
+            <div>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ハワイアンソルト</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ハワイアンマヨネーズ</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ハワイアントマト</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ブルーチーズ</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ハワイアンチョコレート</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>アンチョビ</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>エビ</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ガーリックスライス</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>トロピカルフルーツ</span>
+              </label>
+              <label class="item-topping">
+                <input type="checkbox" />
+                <span>ココナッツ</span>
+              </label>
+            </div>
+          </div>
+          <div class="row item-quantity">
+            <div class="item-hedding item-hedding-quantity">数量</div>
+            <div class="item-quantity-selectbox">
+              <div class="input-field col s12">
+                <select class="browser-default">
+                  <option value="" disabled selected>選択して下さい</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="row item-total-price">
+            <span>この商品金額：38,000 円(税抜)</span>
+          </div>
+          <div class="row item-cart-btn">
+            <button
+              class="btn"
+              type="button"
+              onclick="location.href='cart_list.html'"
+            >
+              <span>カートに入れる</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- end container -->
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class XXXComponent extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
